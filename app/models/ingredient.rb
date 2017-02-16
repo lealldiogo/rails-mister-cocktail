7e49a1e677824f13277e4aab5270e26bf56be1c5
Diogo Loreto Leal <lealldiogo@gmail.com>
2017-02-16 11:42:56 -0200

    Perfect model rake

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,9 @@
 class Ingredient < ApplicationRecord
+  has_many :doses, dependent: :destroy, before_remove: :check_doses_children
+  validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+
+  def check_doses_children
+    self.doses.blank?
+  end
 end
